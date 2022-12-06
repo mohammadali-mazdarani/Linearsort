e178bef02c1bfb1f7267c80abe5f55eaef9c906c
Mohammad ali mazdarani <74422713+mohammadali-mazdarani@users.noreply.github.com>
2022-12-05 22:37:51 -0800

    add function for get number of index array

---
+++ Linearsearch.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system(
+) or input loop */
+/*branch name
+*
+*
+*
+*/
+//prototype
+
+int getindexnu();
+
+
+
+//--------------------------
+//micro
+//---------------------
+//global variable
+//-------------------
+int main(int argc, char** argv)
+{
+
+//Declare number of index of array
+int n;
+
+//Get number index of array 
+n = getindexnu();
+
+
+//function variable
+//--------------------------
+//Functions
+
+//Function for get number index of array
+int getindexnu()
+{
+    int a;
+    cout<<"Enter number of array = ";
+    cin>>a;
+
+    return a;
+}
+
